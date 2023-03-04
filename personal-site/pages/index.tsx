af5c7da0fbc11742ab547fd069f5accfe8eb5009
Will Howard <w.howard256+github@gmail.com>
2023-03-04 18:34:19 +0000

    basic structure

--- personal-site/pages/index.tsx
+++ personal-site/pages/index.tsx
@@ -1,11 +1,50 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { createUseStyles } from "react-jss";
 
-const inter = Inter({ subsets: ['latin'] })
+const COLUMN_MAX_WIDTH = "640px";
+
+const useStyles = createUseStyles((theme: any) => ({
+  main: {
+    minHeight: "100vh",
+    backgroundColor: theme.background,
+  },
+  headerRow: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+  },
+  header: {
+    fontFamily: "Gill Sans,Gill Sans MT,Calibri,sans-serif",
+    color: theme.primary,
+  },
+  crestContainer: {
+    maxHeight: 111,
+    objectFit: "contain",
+  },
+  crest: {
+    position: "relative",
+    height: 111,
+    width: 'auto',
+  },
+  paragraph: {
+    fontFamily:
+      "Petrona, Iowan Old Style, Apple Garamond, Baskerville, Times New Roman, Times, Source Serif Pro, serif",
+    fontSize: 16,
+  },
+  centralColumn: {
+    margin: "auto",
+    maxWidth: COLUMN_MAX_WIDTH,
+    padding: "48px 24px",
+  },
+}));
 
 export default function Home() {
+  const classes = useStyles();
+
+  const imageHeight = 100;
+
   return (
     <>
       <Head>
@@ -14,110 +53,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
+      <main className={classes.main}>
+        <div className={classes.centralColumn}>
+          <div className={classes.headerRow}>
+            <h1 className={classes.header}>Will Howard</h1>
+            <Image height={imageHeight} width={(138 / 111) * imageHeight} src="/crest.png" alt="I just wanted a crest" />
+          </div>
+          <p className={classes.paragraph}>
+            I am a software engineer and effective altruist
           </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
         </div>
       </main>
     </>
-  )
+  );
 }
---
+++ personal-site/styles/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  black: "#000000",
+  background: "#f7f3ef",
+  primary: "#002654", // French flag blue
+//   primary: "#002147", // Oxford blue
+  secondary: "#898880",
+  secondaryLight: "#C5C3BC",
+  accent: "#80722A",
+  accentMid: "#E9E7DE",
+}
